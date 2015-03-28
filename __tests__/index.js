65f270cf295c3b859abb33da91bd767e55b3c9be
Vitaly[AVVS] <v@aminev.me>
2015-03-27 20:49:47 -0700

    Test 2: check local callback queue implementation

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,45 +3,76 @@
 
 jest.autoMockOff();
 
-// stupid jest doesnt work with native module for some reason
-jest.setMock('msgpack', {
-
-    pack: function (obj) {
-        return JSON.stringify(obj);
-    },
-
-    unpack: function (obj) {
-        return JSON.parse(obj);
-    }
-
-});
-
 describe('Distributed callback queue', function () {
 
-    describe('callback queue', function () {
+    it('callback queue: should queue new job request', function () {
+        var callbackQueue = require('../lib/callback-queue.js');
+        var fn = jest.genMockFn();
+        var queue = callbackQueue._queue();
+        var keyname = 'test-queue';
+        var hasQueued = callbackQueue.add(keyname, fn);
 
-        it('should queue new job request', function () {
-            var callbackQueue = require('../lib/callback-queue.js');
-            var fn = jest.genMockFn();
-            var queue = callbackQueue._queue();
-            var keyname = 'test-queue';
-            var hasQueued = callbackQueue.add(keyname, fn);
+        expect(hasQueued).toBe(true);
+        expect(queue[keyname]).toEqual(jasmine.any(Function));
 
-            expect(hasQueued).toBe(true);
-            expect(queue[keyname]).toEqual(jasmine.any(Function));
+        var arg1 = 'yahoo';
+        queue[keyname](null, arg1);
 
-            var arg1 = 'yahoo';
-            queue[keyname](null, arg1);
+        jest.runAllTimers();
 
-            jest.runAllTimers();
-
-            expect(fn).toBeCalledWith(null, arg1);
-            expect(fn.mock.calls.length).toBe(1);
-        });
-
+        expect(fn).toBeCalledWith(null, arg1);
+        expect(fn.mock.calls.length).toBe(1);
     });
 
-    describe('local callback queue', function () {
+    it('local callback queue', function () {
+        var callbackQueue = require('../lib/callback-queue.js');
+        var callback = jest.genMockFn();
+        var callMock = jest.genMockFn().mockImpl(function () {
+            return callbackQueue._call.apply(null, arguments);
+        });
+
+        // mock call
+        callbackQueue._setCall(callMock);
+
+        // vars
+        var queueName = 'test-local';
+        var pubsubChannel = 'test';
+        var redisPublisher = { publish: jest.genMockFn() };
+        var redisConsumer = { subscribe: jest.genMockFn() };
+        var logger = { warn: jest.genMockFn() };
+
+        var consumer = callbackQueue.createConsumer(redisConsumer, pubsubChannel, logger);
+        var publisher = callbackQueue.createPublisher(redisPublisher, pubsubChannel);
+
+        // wire publish to local subscribed channel
+        redisPublisher.publish.mockImpl(consumer);
+
+        var isQueued = callbackQueue.add(queueName, callback);
+
+        // checks queue
+        expect(isQueued).toBe(true);
+        expect(callbackQueue._queue()[queueName]).toEqual(jasmine.any(Function));
+
+        // check subscription
+        expect(redisConsumer.subscribe).toBeCalledWith(pubsubChannel);
+        expect(redisConsumer.subscribe.mock.calls.length).toBe(1);
+
+        var message = { queued: true };
+        publisher(queueName, null, message);
+
+        jest.runAllTimers();
+
+        expect(callbackQueue._queue()[queueName]).toEqual(null);
+        expect(redisPublisher.publish.mock.calls.length).toBe(1);
+        expect(redisPublisher.publish).toBeCalledWith(pubsubChannel, JSON.stringify([ queueName, [ null, message ] ]));
+
+        expect(callback.mock.calls.length).toBe(1);
+        expect(callback).toBeCalledWith(null, message);
+
+        expect(callMock.mock.calls.length).toBe(2);
+        expect(callMock).toBeCalledWith(queueName, [ null, message ]);
+
+        expect(logger.warn.mock.calls.length).toBe(0);
 
     });
 
